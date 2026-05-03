865782d7e4b2809099f1f91703f9223f93657f9c
tseretelinika <tsereelinika@gmail.com>
2026-05-03 21:07:03 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -3,9 +3,48 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+"""
+ლექსიკონიდან ინფორმაციის წამოღება:
+info = profiles[0] -> info არის ლექსიკონი
+print(info.get("name"), info["name"]) -> get method ან პირდაპირ ['key']-ის დახმარებით
+"""
+
+books = [
+    {
+        "id": 1,
+        "title": "Romeo and Juliet",
+        "author": "William Shakespeare",
+        "rating": 4.7,
+        "img": "book1.jpg",
+
+        "description": "Romeo and Juliet is a tragic love story about two young lovers whose families are enemies."
+    },
+
+    {
+        "id": 2,
+        "title": "The Little Prince",
+        "author": "Antoine de Saint-Exupéry",
+        "rating": 4.9,
+        "img": "book2.jpg",
+
+        "description": "The Little Prince tells the story of a young prince exploring planets and learning about life."
+    },
+
+    {
+        "id": 3,
+        "title": "1984",
+        "author": "George Orwell",
+        "rating": 4.8,
+        "img": "book3.jpg",
+
+        "description": "1984 is a dystopian novel about a society controlled by surveillance and propaganda."
+    }
+]
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("about.html")
 
 
 @app.route("/login")
@@ -13,13 +52,30 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/about")
+def index():
+    return render_template("index.html", books=books)
+
+
+
+
+
+@app.route("/book/<int:book_id>")
+def view_book(book_id):
+
+    for book in books:
+        if book["id"] == book_id:
+            return render_template("book_details.html", book=book)
+
+    return "Book Not Found"
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
 
-@app.route("/base")
-def base():
-    return render_template("base.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+
+app.run(debug=True)
